aa66d975fb20eee7ba083d72ba8ad437395948f9
Enes Talay <enestalayy@gmail.com>
2024-07-14 21:14:27 +0300

    Update SwapStore.js

--- stores/SwapStore.js
+++ stores/SwapStore.js
@@ -19,63 +19,63 @@ export const useSwapStore = defineStore("swapStore", {
   }),
   getters: {},
   actions: {
-    async swapTransaction() {
-      const connection = new Connection(
-        "https://neat-hidden-sanctuary.solana-mainnet.discover.quiknode.pro/2af5315d336f9ae920028bbb90a73b724dc1bbed/"
-      );
+    // async swapTransaction() {
+    //   const connection = new Connection(
+    //     "https://neat-hidden-sanctuary.solana-mainnet.discover.quiknode.pro/2af5315d336f9ae920028bbb90a73b724dc1bbed/"
+    //   );
 
-      const accountStore = useAccountStore();
-      // SETUP WALLET
-      const wallet = new Wallet(
-        Keypair.fromSecretKey(accountStore.secretKey || "")
-      );
-      console.log("wallet :>> ", wallet);
+    //   const accountStore = useAccountStore();
+    //   // SETUP WALLET
+    //   const wallet = new Wallet(
+    //     Keypair.fromSecretKey(accountStore.secretKey || "")
+    //   );
+    //   console.log("wallet :>> ", wallet);
 
-      // Swapping SOL to USDC with input 0.1 SOL and 0.5% slippage
-      const quoteResponse = await (
-        await $fetch(
-          "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qZhg4c9W3FJJia3qcF1kNJY2VF&amount=1"
-        )
-      ).json();
-      console.log({ quoteResponse });
+    //   // Swapping SOL to USDC with input 0.1 SOL and 0.5% slippage
+    //   const quoteResponse = await (
+    //     await $fetch(
+    //       "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qZhg4c9W3FJJia3qcF1kNJY2VF&amount=1"
+    //     )
+    //   ).json();
+    //   console.log({ quoteResponse });
 
-      // get serialized transactions for the swap
-      const { swapTransaction } = await (
-        await fetch("https://quote-api.jup.ag/v6/swap", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            // quoteResponse from /quote api
-            quoteResponse,
-            // user public key to be used for the swap
-            userPublicKey: wallet.publicKey.toString(),
-            // auto wrap and unwrap SOL. default is true
-            wrapAndUnwrapSol: true,
-            // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-            // feeAccount: "fee_account_public_key"
-          }),
-        })
-      ).json();
+    //   // get serialized transactions for the swap
+    //   const { swapTransaction } = await (
+    //     await fetch("https://quote-api.jup.ag/v6/swap", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         // quoteResponse from /quote api
+    //         quoteResponse,
+    //         // user public key to be used for the swap
+    //         userPublicKey: wallet.publicKey.toString(),
+    //         // auto wrap and unwrap SOL. default is true
+    //         wrapAndUnwrapSol: true,
+    //         // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+    //         // feeAccount: "fee_account_public_key"
+    //       }),
+    //     })
+    //   ).json();
 
-      // deserialize the transaction
-      const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
-      var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-      console.log(transaction);
+    //   // deserialize the transaction
+    //   const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
+    //   var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+    //   console.log(transaction);
 
-      // sign the transaction
-      transaction.sign([wallet.payer]);
+    //   // sign the transaction
+    //   transaction.sign([wallet.payer]);
 
-      // Execute the transaction
-      const rawTransaction = transaction.serialize();
-      const txid = await connection.sendRawTransaction(rawTransaction, {
-        skipPreflight: true,
-        maxRetries: 2,
-      });
-      await connection.confirmTransaction(txid);
-      console.log(`https://solscan.io/tx/${txid}`);
-    },
+    //   // Execute the transaction
+    //   const rawTransaction = transaction.serialize();
+    //   const txid = await connection.sendRawTransaction(rawTransaction, {
+    //     skipPreflight: true,
+    //     maxRetries: 2,
+    //   });
+    //   await connection.confirmTransaction(txid);
+    //   console.log(`https://solscan.io/tx/${txid}`);
+    // },
   },
 });
 
